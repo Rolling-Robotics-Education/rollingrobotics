7c0e180a27cb517740fd073b0664b2b2e07bf959
E12345EE12345E <60201680+E12345EE12345E@users.noreply.github.com>
2024-08-02 15:43:39 -0700

    logo + impact

--- app/js/main.js
+++ app/js/main.js
@@ -49,7 +49,7 @@ document.getElementById("sendtotop").addEventListener('click', function() {
         top: y,
         behavior: 'smooth'
     });
-})
+});
 
 document.getElementById("sendtoaboutus").addEventListener('click', function() {
     let y = document.getElementById("aboutus").getBoundingClientRect().top + window.scrollY - TOP_BAR_HEIGHT - SCROLLPADDING;
@@ -57,7 +57,15 @@ document.getElementById("sendtoaboutus").addEventListener('click', function() {
         top: y,
         behavior: 'smooth'
     });
-})
+});
+
+document.getElementById("sendtoimpact").addEventListener('click', function() {
+    let y = document.getElementById("impact").getBoundingClientRect().top + window.scrollY - TOP_BAR_HEIGHT - SCROLLPADDING;
+    window.scroll({
+        top: y,
+        behavior: 'smooth'
+    });
+});
 
 // lerp
 
