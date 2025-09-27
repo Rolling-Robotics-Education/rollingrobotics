8747544f8fa80cda8027111a6ee1c4e3fddadfe8
Shengyu Fu <shengyfu@microsoft.com>
2025-09-26 20:54:17 -0700

    Add domain redirection

--- index.js
+++ index.js
@@ -10,6 +10,14 @@ const PORT = process.env.PORT || 8080;
 // Serve static files from the 'public' directory
 app.use(express.static(path.join(__dirname, 'app')));
 
+app.use((req, res, next) => {
+  const host = req.headers.host?.split(':')[0];
+  if (host && host.toLowerCase() === 'rollingrobotics.org') {
+    return res.redirect(301, 'https://www.rollingrobotics.org' + req.originalUrl);
+  }
+  next();
+});
+
 // Handle all other routes by serving index.html (for single-page applications)
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'app', 'index.html'));
@@ -19,3 +27,4 @@ app.get('*', (req, res) => {
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
+
